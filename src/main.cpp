d08f3d2478783d5a531baa7958d5ca2224974730
Sonia-Nemo <log.sonia.club@etsmtl.ca>
2024-04-28 01:04:11 -0400

    mock dvl for driver test

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "sonia_common_cpp/EthernetSocket.h"
+#include <stdlib.h>
+#include <iostream>
+#include <chrono>
+
+int main()
+{
+    sonia_common_cpp::EthernetSocket socket = sonia_common_cpp::EthernetSocket();
+    socket.Connect("192.168.0.32", 9002);
+
+    std::vector<uint8_t> data = socket.GetRawData();
+
+    std::string str(data.begin(), data.end());
+
+    std::cout << str << std::endl;
+    return EXIT_SUCCESS;
+}
