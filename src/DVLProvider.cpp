200bcd64f763a8d2f29a1b06349363728c943296
AUV8 <log.club.sonia@etsmtl.ca>
2025-07-30 12:48:22 -0400

    cleanup

--- src/DVLProvider.cpp
+++ src/DVLProvider.cpp
@@ -23,8 +23,6 @@ namespace dvl_port_manager
     
     DVLProvider::~DVLProvider()
     {
-        if(_receiveThread.joinable())
-            _receiveThread.join();
     }
 
 } // namespace dvl_port_manager
